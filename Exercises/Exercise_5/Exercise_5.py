9d6cf25869b09c74a88840478e3253413ba696ad
jungian6 <122241019+jungian6@users.noreply.github.com>
2024-07-24 06:38:07 +0100

    Added test files

--- Exercises/Exercise_5/Exercise_5.py
+++ Exercises/Exercise_5/Exercise_5.py
@@ -1,28 +1,26 @@
-# This program converts a numeric grade to an alpha grade
-
 input_bool = False
 
 while not input_bool:
   input_grade = int(input("Please enter the grade you wish to convert (0-100).\n"))
-  if (input_grade >= 0) or (input_grade <= 100):
+  if (input_grade >= 0) and (input_grade <= 100):
     input_bool = True
-
-  print("ERROR: input not between 0 and 100\n")
+  else:
+    print("ERROR: input not between 0 and 100\n")
 
 output_grade = ""
-if input_grade <= 35:
+if input_grade < 35:
   output_grade = "F"
-elif input_grade <= 40:
+elif input_grade < 40:
   output_grade = "MF"
-elif input_grade <= 50:
+elif input_grade < 50:
   output_grade = "D"
-elif input_grade <= 60:
+elif input_grade < 60:
   output_grade = "C"
-elif input_grade <= 70:
+elif input_grade < 70:
   output_grade = "B"
-elif input_grade <= 80:
+elif input_grade < 80:
   output_grade = "A-"
-elif input_grade <= 90:
+elif input_grade < 90:
   output_grade = "A"
 else:
   output_grade = "A+"
---
+++ Lessons/Lesson_4/L4_booleans.py
@@ -0,0 +1,11 @@
+# DO NOT TOUCH THESE LINES. THEY ARE USED BY THE TESTS
+# If you want to test this program, when you hit Run, enter values of True or False via the console
+is_raining = bool(int(input()))
+no_hat = bool(int(input()))
+# ###################################################### e.g takes_umbrella = is_raining or no_hat You should fix
+# this line to by forming an expression using is_raining and no_hat to produce the correct result for takes_umbrella.
+# Note you only have to fix this line to pass the tests. No if statements etc.. required!
+
+takes_umbrella = True
+
+print(takes_umbrella)
